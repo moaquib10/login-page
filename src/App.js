9f4656fdc2f1a3715e2c626eeeeb12bb342bab60
moaquib10 <moaquib10@gmail.com>
2023-03-18 18:12:53 +0530

    adding new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <h1>Create Account</h1>
+        <div>
+          <label>Email</label>
+          <input type= 'text' name = 'email' placeholder= 'example@mail.com' />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type= 'password' name = 'password' placeholder= 'choose secure password' />
+        </div>
+        <div>
+          <label>Re-Enter New Password</label>
+          <input type= 'password' name = 're-enter new password' placeholder= 'choose secure password' />
+        </div>
+        <div>
+          <button type= 'button reset submit' name= 'login_button'>Create Account</button>
+        </div>
+      </form>
     </div>
+
   );
 }
 
-export default App;
